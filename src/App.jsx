89f0f4d908f895755208e3394a805a46129e69e2
Aditya Girdhar <92713380+adityagirdhar@users.noreply.github.com>
2023-03-10 20:09:00 +0530

    removed blob

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import './App.css';
 function App() {
   return (
     <div className='light'>
-      <div id="blob"></div>
-      <div id="blur"></div>
       <Navbar/>
       <div className="body">
         <Home/>
