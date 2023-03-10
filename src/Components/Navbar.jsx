585df567ea8c4103aac7cf3d1ccdb5f8edee76ac
Aditya Girdhar <92713380+adityagirdhar@users.noreply.github.com>
2023-03-10 16:15:08 +0530

    added navbar container

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import '../Styles/Navbar.css'
+
+function Navbar() {
+    return (
+        <nav>
+            <img alt="airbnb-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/1200px-Airbnb_Logo_B%C3%A9lo.svg.png"/>
+        </nav>
+    )
+};
+
+export default Navbar;
